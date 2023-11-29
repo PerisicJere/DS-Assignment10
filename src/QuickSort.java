3eee628c0ada02bb928fd2da113129fc12156850
PerisicJere <jereperisic@icloud.com>
2023-11-28 22:42:22 -0500

    Added second partition, and included method for experiment1 (partition1)

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -3,34 +3,66 @@ import java.util.ArrayList;
 public class QuickSort {
 
 
-    public QuickSort(ArrayList<SaleRecord> saleRecords){
-            sort(saleRecords,0,saleRecords.size()-1);
-            }
-    public void sort(ArrayList<SaleRecord> saleRecords,int begin,int end){
+    public QuickSort(ArrayList<SaleRecord> saleRecords, int partitionNum){
+            sort(saleRecords,0,saleRecords.size()-1, partitionNum);
+    }
+    public void sort(ArrayList<SaleRecord> saleRecords,int begin,int end, int partitionNum){
             if(begin<end){
-            int partitionIndex = partition1(saleRecords,begin,end);
-
-            sort(saleRecords,begin,partitionIndex-1);
-            sort(saleRecords,partitionIndex+1,end);
+                int partitionIndex = 0;
+                if(partitionNum == 1) {
+                    partitionIndex = partition1(saleRecords, begin, end);
+                } else if(partitionNum == 2){
+                    partitionIndex = partition2(saleRecords, begin, end);
+                }else if(partitionNum == 3){
+                    partitionIndex = 0;//partition3(saleRecords, begin, end);
+                }else if(partitionNum == 4){
+                    partitionIndex = 0;//partition4(saleRecords, begin, end);
+                }else if(partitionNum == 5){
+                    partitionIndex = 0;//partition5(saleRecords, begin, end);
+                }else if(partitionNum == 6){
+                    partitionIndex = 0;//partition6(saleRecords, begin, end);
+                }
+            sort(saleRecords,begin,partitionIndex-1, partitionNum);
+            sort(saleRecords,partitionIndex+1,end, partitionNum);
             }
         }
-    private int partition1(ArrayList<SaleRecord> saleRecords,int begin,int end){
-            String pivot=saleRecords.get(end).getDate();
-            int i=begin-1;
-            for(int j=begin;j<end; j++){
-            if(saleRecords.get(j).getDate().compareTo(pivot)<=0){
-            i++;
+    private int partition1(ArrayList<SaleRecord> saleRecords, int begin, int end){
+        String pivot = saleRecords.get(end).getDate();
+        int i = begin - 1;
 
-            SaleRecord temp=saleRecords.get(i);
-            saleRecords.set(i,saleRecords.get(j));
-            saleRecords.set(j,temp);
+        for(int j = begin; j <= end; j++){
+            if(saleRecords.get(j).getDate().compareTo(pivot) > 0){
+                i++;
+
+                SaleRecord temp = saleRecords.get(i);
+                saleRecords.set(i, saleRecords.get(j));
+                saleRecords.set(j, temp);
+            }
+        }
+        SaleRecord temp = saleRecords.get(i+1);
+        saleRecords.set(i+1, saleRecords.get(end));
+        saleRecords.set(end, temp);
+
+        return i;
+    }
+    private int partition2(ArrayList<SaleRecord> saleRecords,int begin,int end){
+            String pivot = saleRecords.get(end).getDate();
+            int i=begin - 1;
+            for(int j=begin; j<end; j++){
+                if(saleRecords.get(j).getDate().compareTo(pivot) >= 0){
+                i++;
+
+                SaleRecord temp=saleRecords.get(i);
+                saleRecords.set(i,saleRecords.get(j));
+                saleRecords.set(j,temp);
                 }
             }
-            SaleRecord temp=saleRecords.get(i+1);
+            SaleRecord temp = saleRecords.get(i+1);
             saleRecords.set(i+1,saleRecords.get(end));
             saleRecords.set(end,temp);
 
             return i+1;
         }
+
 
 }
--- src/program10.java
+++ src/program10.java
@@ -1,21 +1,29 @@
 import java.io.*;
-import java.text.ParseException;
-
 import java.util.*;
 
 public class program10 {
     public static void main(String[] args) {
-        String file = "car_sales_data.csv";
+        String file = "small_sample.csv";
         TreeMap<String, ArrayList<SaleRecord>> treeMap = new TreeMap<>();
         long startTime = System.currentTimeMillis();
         insertFromCSV(file, treeMap);
-        long endTime = System.currentTimeMillis();
-        System.out.println((float)(endTime-startTime)/1000+" seconds");
-        System.out.print("Unsorted list: ");
-
+        experiment1(treeMap);
 
     }
-    private static SaleRecord parsingCSV(String line) throws ParseException {
+    public static void experiment1(TreeMap<String, ArrayList<SaleRecord>> treeMap){
+        System.out.print("Unsorted list: ");
+        ArrayList<SaleRecord> tries = new ArrayList<>();
+        for (Map.Entry<String, ArrayList<SaleRecord>> entry : treeMap.entrySet()) {
+            tries.addAll(entry.getValue());
+        }
+        System.out.println();
+        QuickSort quickSort = new QuickSort(tries,1);
+        System.out.print("Sorted list: ");
+        for(SaleRecord num : tries){
+            System.out.print(num+", ");
+        }
+    }
+    private static SaleRecord parsingCSV(String line) {
         String[] parts = line.split(",");
         if (parts.length != 9) {
             return null;
@@ -36,23 +44,21 @@ public class program10 {
                     continue;
                 }
                 SaleRecord saleRecord = parsingCSV(line);
-                if (saleRecord != null) {
-                    String carMake = saleRecord.getCarMake();
-                    ArrayList<SaleRecord> arrayList = treeMap.get(carMake);
-
-                    if (arrayList == null) {
-                        arrayList = new ArrayList<>();
-                        treeMap.put(carMake, arrayList);
-                    }
-
-                    arrayList.add(saleRecord);
-
-                }
-
+                populateTreeMap(saleRecord, treeMap);
             }
-
-        } catch (IOException | ParseException e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    private static void populateTreeMap(SaleRecord saleRecord, TreeMap<String, ArrayList<SaleRecord>> treeMap){
+        if (saleRecord != null) {
+            String carMake = saleRecord.getCarMake();
+            ArrayList<SaleRecord> arrayList = treeMap.get(carMake);
+            if (arrayList == null) {
+                arrayList = new ArrayList<>();
+                treeMap.put(carMake, arrayList);
+            }
+            arrayList.add(saleRecord);
         }
     }
 }
